8bc4068195ed617a66db5c63617fc807a2fb769c
Haneul Cho <stuykr@naver.com>
2024-12-11 18:49:06 +0900

    build: build 결과물 import 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,6 @@ export default defineConfig(({ mode }) => {
       tsconfigPaths(),
       dts({
         include: ['src'],
-        staticImport: true,
         insertTypesEntry: true,
         rollupTypes: true
       })
